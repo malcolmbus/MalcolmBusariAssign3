894ea1478214a9421bc50266c4171faf8ec3058d
Malcolm Busari <busarim@shernet.sheridancollege.ca>
2021-03-24 03:18:13 -0400

    Set up bottom navigation bar and created empty fragments

--- MalcolmAssign33/src/main/java/malcolm/busari/s991523264/MalcolmActivity.java
+++ MalcolmAssign33/src/main/java/malcolm/busari/s991523264/MalcolmActivity.java
@@ -9,11 +9,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MalcolmActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.malcolmNavView);
     }
 }
