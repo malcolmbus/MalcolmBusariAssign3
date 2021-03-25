dd61cd6ca8f62b9abdc8e401c6a3c772ea6e367f
Malcolm Busari <busarim@shernet.sheridancollege.ca>
2021-03-25 03:09:22 -0400

    Created a button and implemented an onclick event to access the camera with runtime permission. Created a start and stop button for the animation

--- MalcolmAssign33/src/main/java/malcolm/busari/s991523264/HomeFragment.java
+++ MalcolmAssign33/src/main/java/malcolm/busari/s991523264/HomeFragment.java
@@ -1,12 +1,25 @@
 package malcolm.busari.s991523264;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +27,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+
+    private static final int CAMERA_PERMISSION_CODE = 100;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +74,64 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        final int pic_id = 123;
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        Button cameraBtn = view.findViewById(R.id.malcolmCameraBtn);
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(checkPermission()) {
+                    Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(camera_intent, pic_id);
+                } else {
+                    requestPermission();
+                }
+            }
+        });
+        return view;
     }
+
+    public boolean checkPermission() {
+
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[]{Manifest.permission.CAMERA},
+                CAMERA_PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super
+                .onRequestPermissionsResult(requestCode,
+                        permissions,
+                        grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getActivity(),
+                        "Camera Permission Granted",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                Snackbar snackbar = Snackbar.make(getActivity().findViewById(android.R.id.content),
+                        "Camera Permission Denied", Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+        }
+    }
+
 }
