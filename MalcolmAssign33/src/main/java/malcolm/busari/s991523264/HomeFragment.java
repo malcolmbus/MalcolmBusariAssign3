2bc0b3880d3e0f3aeaf31a87b080b4656815ffa4
Malcolm Busari <busarim@shernet.sheridancollege.ca>
2021-03-25 15:06:10 -0400

    Set up example animation to test and created startAnimation and stopAnimation methods

--- MalcolmAssign33/src/main/java/malcolm/busari/s991523264/HomeFragment.java
+++ MalcolmAssign33/src/main/java/malcolm/busari/s991523264/HomeFragment.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -17,6 +19,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -29,6 +32,8 @@ import com.google.android.material.snackbar.Snackbar;
 public class HomeFragment extends Fragment {
 
     private static final int CAMERA_PERMISSION_CODE = 100;
+    AnimationDrawable mframeAnimation = null;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -78,20 +83,24 @@ public class HomeFragment extends Fragment {
         final int pic_id = 123;
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         Button cameraBtn = view.findViewById(R.id.malcolmCameraBtn);
-        cameraBtn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if(checkPermission()) {
-                    Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(camera_intent, pic_id);
-                } else {
-                    requestPermission();
-                }
+        cameraBtn.setOnClickListener(v -> {
+            if(checkPermission()) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(camera_intent, pic_id);
+            } else {
+                requestPermission();
             }
         });
+
+        Button startBtn = view.findViewById(R.id.malcolmStartBtn);
+        startBtn.setOnClickListener(v -> startAnimation());
+
+        // Handle Stop Button
+        Button stopBtn = view.findViewById(R.id.malcolmStopBtn);
+        stopBtn.setOnClickListener(v -> stopAnimation());
         return view;
     }
+
 
     public boolean checkPermission() {
 
@@ -132,6 +141,44 @@ public class HomeFragment extends Fragment {
                 snackbar.show();
             }
         }
+    }
+
+    private void startAnimation()
+    {
+
+        ImageView img = getView().findViewById(R.id.malcolmHomeIV);
+
+        BitmapDrawable frame1 = (BitmapDrawable)getResources().getDrawable(R.drawable.boy1);
+        BitmapDrawable frame2 = (BitmapDrawable)getResources().getDrawable(R.drawable.boy2);
+        BitmapDrawable frame3 = (BitmapDrawable)getResources().getDrawable(R.drawable.boy3);
+        BitmapDrawable frame4 = (BitmapDrawable)getResources().getDrawable(R.drawable.boy4);
+        BitmapDrawable frame5 = (BitmapDrawable)getResources().getDrawable(R.drawable.boy5);
+        BitmapDrawable frame6 = (BitmapDrawable)getResources().getDrawable(R.drawable.boy6);
+
+        // Get the background, which has been compiled to an AnimationDrawable object.
+        int reasonableDuration = 250;
+        mframeAnimation = new AnimationDrawable();
+        mframeAnimation.setOneShot(false);	// loop continuously
+        mframeAnimation.addFrame(frame1, reasonableDuration);
+        mframeAnimation.addFrame(frame2, reasonableDuration);
+        mframeAnimation.addFrame(frame3, reasonableDuration);
+        mframeAnimation.addFrame(frame4, reasonableDuration);
+        mframeAnimation.addFrame(frame5, reasonableDuration);
+        mframeAnimation.addFrame(frame6, reasonableDuration);
+        mframeAnimation.addFrame(frame6, reasonableDuration);
+        mframeAnimation.addFrame(frame6, reasonableDuration);
+
+        img.setBackground(mframeAnimation);
+
+        mframeAnimation.setVisible(true,true);
+        mframeAnimation.start();
+    }
+
+    private void stopAnimation()
+    {
+        mframeAnimation.stop();
+        mframeAnimation.setVisible(false,false);
+
     }
 
 }
